67d78827a42fb2896f5e3b1082f1df23d9928c2f
asadkhalid566 <76807556+asadkhalid566@users.noreply.github.com>
2024-07-08 06:24:56 +0500

    up

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // Note the change here
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-const rootElement = document.getElementById('root');
-const root = ReactDOM.createRoot(rootElement); // Create a root
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
 reportWebVitals();
