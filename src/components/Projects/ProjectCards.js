ffc231ebeec9b52a343cbecfd525ed512b35d3bf
Mohsin1016 <muhammadmohsin1016@gmail.com>
2025-01-29 13:15:51 +0500

    new-animations

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -8,10 +8,8 @@ function ProjectCards(props) {
     <Card className="project-card-view">
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
       <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }}>
-          {props.description}
-        </Card.Text>
+        <Card.Title className="project-card-title">{props.title}</Card.Title>
+        <Card.Text className="project-card-text">{props.description}</Card.Text>
         {!props.isBlog && props.demoLink && (
           <Button
             variant="primary"
@@ -19,8 +17,7 @@ function ProjectCards(props) {
             target="_blank"
             style={{ marginLeft: "10px" }}
           >
-            <CgWebsite /> &nbsp;
-            {"Demo"}
+            <CgWebsite /> &nbsp;{"Demo"}
           </Button>
         )}
       </Card.Body>
