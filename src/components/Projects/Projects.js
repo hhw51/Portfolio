a3a0b3d33d2143216def98d11fc33ef1bd18a8dc
Mohsin1016 <muhammadmohsin1016@gmail.com>
2025-02-10 16:02:05 +0500

    project-page

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import projectArray from "../ProjectArray";
-import MobileProjectArray from "../MobileProjectArray";
-import MobileProjectCard from "./MobileProjectCard";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 
@@ -32,30 +30,6 @@ function Projects() {
           ))}
         </Row>
       </Container>
-
-      <Container>
-        <h1 className="project-heading">
-          Mobile <strong className="purple">Development </strong>
-        </h1>
-        <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
-        </p>
-        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          {MobileProjectArray.map((project, index) => (
-            <Col key={index} md={4} className="project-card">
-              <MobileProjectCard
-                imgPath={project.imgPath}
-                isBlog={false}
-                title={project.title}
-                description={project.description}
-                ghLink={project.ghLink}
-                demoLink={project.demoLink}
-              />
-            </Col>
-          ))}
-        </Row>
-      </Container>
-
     </Container>
   );
 }
